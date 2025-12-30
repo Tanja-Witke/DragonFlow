3bd7aa80c4a8cefbdb1cf8ce47a70ccbc085b962
Tanja-Witke <tanja.nayria@gmail.com>
2025-12-30 12:04:52 +1000

    Added Github deploy setup

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+const repoBase = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'DragonFlow';
+
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? `/${repoBase}/` : '/',
   plugins: [
     vue(), 
     tailwindcss(),
